89102e7c531520f587e070648a3facd15a39b2f5
Valerio Billi <valerio.billi18@gmail.com>
2023-07-12 17:38:57 +0200

    dashboard view

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController; 
 
 /*
 |--------------------------------------------------------------------------
@@ -17,8 +18,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth'])
+ 	->prefix('admin') //definisce il prefisso "admin/" per le rotte di questo gruppo
+    ->namespace('Admin') // definisce il namespace per i Controller chiamati in questo gruppo
+ 	->name('admin.') //definisce il pattern con cui generare i nomi delle rotte cioè "admin.qualcosa"
+ 	->group(function () {
+ 	
+ 		//Siamo nel gruppo quindi:
+ 		// - il percorso "/" diventa "admin/"
+ 		// - il nome della rotta ->name("dashboard") diventa ->name("admin.dashboard")
+ 		Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
+ });
 
 require __DIR__.'/auth.php';
+
