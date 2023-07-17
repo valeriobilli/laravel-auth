1757c75083e6b501df7e6ee61b10200d4ef05c11
Valerio Billi <valerio.billi18@gmail.com>
2023-07-17 02:03:54 +0200

    feat crud delete

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -99,6 +99,8 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        $post->delete();
+
+        return redirect()->route('admin.posts.index');
     }
 }
